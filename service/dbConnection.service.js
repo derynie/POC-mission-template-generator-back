4c5dc6afb1f8e1ff199abcf3b3d08519eb38b341
quentin <quentin.journet@epitech.eu>
2019-08-07 14:22:46 +0200

    fix env

--- service/dbConnection.service.js
+++ service/dbConnection.service.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
+const { bddHost, bddUser, bddPassword, bddDatabase } = require('../utils/config');
 
 const pool      =    mysql.createPool({
     connectionLimit : 100,
-    host     : 'localhost',
-    user     : 'quentin',
-    password : 'toto1705//',
-    database : 'missions',
+    host     : bddHost,
+    user     : bddUser,
+    password : bddPassword,
+    database : bddDatabase,
     debug    :  false
 });
 
